da8ac97f7d576d757bd18930722b43b717cf5890
FernanDeHoyos11 <fernandehoyos93@gmail.com>
2023-10-23 15:51:41 -0500

    add init config from calendarApp-Mern

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,14 +6,6 @@ const crearUsuario = (req, res = response) => {
 
     const { name, email, password } = req.body;
 
-    const errors = validationResult(req)
-
-    if(!errors.isEmpty()){
-        return res.status(400).json({
-            ok: false,
-            errors: errors.mapped()
-        })
-    }
 
     res.status(200).json({
         msg: 'Registro',
@@ -29,15 +21,6 @@ const crearUsuario = (req, res = response) => {
 const loginUsuario = (req, res = response) => {
 
     const {email, password} = req.body
-
-    const errors = validationResult(req)
-
-    if(!errors.isEmpty()){
-        return res.status(400).json({
-            ok: false,
-            errors: errors.mapped()
-        })
-    }
 
 
     res.json({
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const {Router} = require('express')
 const {check} = require('express-validator')
 const {crearUsuario, loginUsuario, renovarToken} = require('../controllers/auth')
+const { validarcampos } = require('../middlewares/validar_campos')
 const router = Router()
 
 
@@ -9,7 +10,8 @@ router.post(
     [
      check('name', 'El nombre es obligatorio').not().isEmpty(),
      check('email', 'El email es obligatorio').isEmail(),
-     check('password', 'la contraseña debe tener mas de 6 letras').isLength({min: 6})
+     check('password', 'la contraseña debe tener mas de 6 letras').isLength({min: 6}),
+     validarcampos
     ],
     crearUsuario )
 
@@ -18,7 +20,8 @@ router.post(
     '/login',
     [
         check('email', 'El email es obligatorio').isEmail(),
-        check('password', 'la contraseña debe tener mas de 6 letras').not().isEmpty()
+        check('password', 'la contraseña debe tener mas de 6 letras').not().isEmpty(),
+        validarcampos
     ],
      loginUsuario )
 router.get('/renew', renovarToken )
